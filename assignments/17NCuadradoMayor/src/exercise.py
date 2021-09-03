c60bf243218ce25833269983e7c5c109b82ed667
Varuneishon <89056489+Varuneishon@users.noreply.github.com>
2021-09-03 14:45:37 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -2,6 +2,13 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    n = int(input("Escribe un numero: "))
+x=1
+a=[]
+for i in range(n+1):
+        if i**2 >= n:
+                a.append(i)
+                print(min(a))
     pass
 
 if __name__=='__main__':
