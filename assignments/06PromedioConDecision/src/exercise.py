7fd0a55ee136d1ea34c4bce4829f4de756f9b7c0
Varuneishon <89056489+Varuneishon@users.noreply.github.com>
2021-09-06 20:10:00 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,14 +1,23 @@
 def main():
     #escribe tu código abajo de esta línea
-    n = int(input('How many numbers: '))
-t = 0
-	
-for i in range(0,n):
-	num = float(input('Enter number : '))
-t += num
-	
-avg = t/n
-print('Average of ', n, ' numbers is :', avg)
+   i = float(input('Please enter a number: '))
+s = 0
+c = 0
+
+if i>0:
+    c=1
+    t=0
+    while t>=0:
+        i1 = float(input('Please enter another number: '))
+        t=i1
+        i=i+i1
+        c=c+1
+        a=i/c
+
+
+    print(a)
+else:
+    print('no es valido')
     pass
 if __name__=='__main__':
     main()
