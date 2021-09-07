4d4198595f782e45973aedf8b1578b6e5b3cc5dc
Varuneishon <89056489+Varuneishon@users.noreply.github.com>
2021-09-06 20:31:09 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,6 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-   i = float(input('Please enter a number: '))
+i = float(input('Please enter a number: '))
 s = 0
 c = 0
 
@@ -9,12 +9,14 @@ if i>0:
     t=0
     while t>=0:
         i1 = float(input('Please enter another number: '))
+        if i1<0:
+            break
         t=i1
         i=i+i1
         c=c+1
-        a=i/c
 
 
+    a=i/c
     print(a)
 else:
     print('no es valido')
